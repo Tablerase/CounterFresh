05f7f4318214c36ab296bda6d7434d1cff0c16ea
rcutte <rcutte@student.42.fr>
2026-08-11 18:58:33 +0200

    feat: animation for minus arrow

---
+++ components/MinusIcon.tsx
@@ -0,0 +1,63 @@
+import "./MinusIcon.css";
+
+export default function MinusIcon() {
+  return (
+    <svg
+      width="500"
+      height="500"
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+      id="minus-box"
+    >
+      <rect width="32" height="32" fill="url(#grid-pattern)" />
+      <g id="minus-core">
+        <line
+          stroke-linecap="round"
+          x1={10}
+          y1={16}
+          x2={22}
+          y2={16}
+        />
+        <line
+          id="arrow-top"
+          stroke-linecap="round"
+          x2={22}
+          y2={12}
+          x1={26}
+          y1={16}
+        />
+        <line
+          id="arrow-bottom"
+          stroke-linecap="round"
+          x1={26}
+          y1={16}
+          x2={22}
+          y2={20}
+        />
+        {/* <path */}
+        {/*   id="minus-arrow-head" */}
+        {/*   d=" M 22 12 L 26 16 */}
+        {/*   L 22 20 */}
+        {/*   " */}
+        {/*   fill="none" */}
+        {/*   stroke-linecap="round" */}
+        {/*   stroke-linejoin="round" */}
+        {/* /> */}
+      </g>
+      <defs>
+        <pattern
+          id="grid-pattern"
+          width="1"
+          height="1"
+          stroke-width="0.1"
+          patternUnits="userSpaceOnUse"
+          stroke="grey"
+        >
+          <rect width="1" height="1" x="0" y="0" fill="white" />
+          <path d=" M 1 0 L 1 1
+                  M 0 1 L 1 1" />
+        </pattern>
+      </defs>
+    </svg>
+  );
+}
---
+++ islands/Participant.tsx
@@ -0,0 +1,29 @@
+import { useSignal } from "@preact/signals";
+import Counter from "@/islands/Counter.tsx";
+import Surface from "@/components/Surface.tsx";
+import { useId } from "preact/hooks";
+
+export default function Participant() {
+  const count = useSignal<number>(0);
+  const name = useSignal<string>("");
+
+  return (
+    <Surface>
+      <input
+        id={useId()}
+        type="text"
+        value={name}
+        placeholder="Name"
+        class={`text-center font-mono text-2xl font-bold text-purple-100 tracking-wider mb-2`}
+      />
+      <Counter count={count} />
+      {
+        /* TODO: Position relative to other participant
+            <p class="font-mono text-xs text-purple-400 tracking-widest uppercase mt-4">
+              connecting to gateway...
+            </p>
+        */
+      }
+    </Surface>
+  );
+}
